bde42788c8d27207833bec9cd15a571bd64ea192
doctamayot <doctamayot@hotmail.com>
2019-02-19 07:25:01 -0500

    sqlite

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::ProductsController < ApplicationController
 protect_from_forgery with: :null_session
+before_action :set_product, only: [:edit, :update, :destroy, :show]
 
   def index
     @products = Product.all
     render json: @products
+  end
+
+  def show
+  	    render json: @product, status: 200
   end
 
   def new
@@ -20,8 +25,30 @@ def create
     end
   end
 
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  
+  def destroy
+    @product.destroy
+    if @product.delete
+      render json: @product, status: 204, location: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+
 
   private
+  def set_product
+      @product = Product.find(params[:id])
+    end
     def product_params
       params.require(:product).permit(:name, :price)
     end
